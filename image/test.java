f895302728888add9a3fe8d7c52d3a3ee3468274
mosooKim <ahtn21@gmail.com>
2022-04-01 10:33:11 +0900

    test

---
+++ image/test.java
@@ -0,0 +1,7 @@
+package image;
+
+public class test {
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
---
+++ 쿼리/test.java
@@ -0,0 +1,7 @@
+package 쿼리;
+
+public class test {
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
