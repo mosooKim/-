d2a5a4081e12060726e0e2fcb14b534ac97d7612
mosooKim <ahtn21@gmail.com>
2022-04-01 09:56:01 +0900

    category

---
+++ java/Main.java
@@ -0,0 +1,10 @@
+package java;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello");
+        System.out.println("hello2");
+        System.out.println("hello3");
+    }
+}
