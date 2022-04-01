b15d701862fc0a500c35e42c8e52cffe1c9e5b5b
mosooKim <ahtn21@gmail.com>
2022-04-01 10:28:04 +0900

    test

--- javaspace/Main.java
+++ javaspace/Main.java
@@ -1,10 +1,9 @@
-package java;
+package javaspace;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println("hello2");
-        System.out.println("hello3");
     }
 }
